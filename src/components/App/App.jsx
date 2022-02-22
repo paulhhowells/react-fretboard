1f3ab31599bbb764f9ff5f90f8c76d08ccd769b9
Paul H Howells <p.h.howells@gmail.com>
2022-02-22 14:14:01 +0000

    refactor: move ControlPlate into its own file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ControlPlate from '../ControlPlate';
 import Neck from '../Neck';
 import { useNotes } from '../../hooks';
 
@@ -10,40 +11,6 @@ function App () {
 			<h1>React Fretboard</h1>
 			<ControlPlate />
 			<Neck openStringPitches={openStringPitches} notes={notes} />
-		</div>
-	);
-}
-
-export function ControlPlate () {
-	return (
-		<div className="control-plate">
-			{/*
-				Key Chooser
-				Vanilla / Blues - Mode Chooser
-				Nashville / Alphabetical - Chooser
-				Tuning chooser - spanish / Eb / dadgad / dadgbd / mandolin / E9 (with pedals) / C6
-				String muting
-
-				Vanilla / Diatonic Mode
-				Triad I - VII
-				Seventh Chords I - VII
-				Diatonic scale / modes I - VII
-				Pentatonic I - VI
-
-				Parallel Sixths - string pair chooser E A D
-
-				Blues Mode - major / minor
-				I / IV / V
-				X Triad
-				X Dominant 7th
-				X Mixolydian
-				Pentatonic
-					X Major Pentatonic / Hexatonic
-					X Minor Pentatonic / Hexatonic
-
-					optional pentatonics I IV V
-				X Diad 3 & 7
-			*/}
 		</div>
 	);
 }
---
+++ src/components/ControlPlate.jsx
@@ -0,0 +1,34 @@
+export default function ControlPlate () {
+	return (
+		<div className="control-plate">
+			Control Plate
+			{/*
+				Key Chooser
+				Vanilla / Blues - Mode Chooser
+				Nashville / Alphabetical - Chooser
+				Tuning chooser - spanish / Eb / dadgad / dadgbd / mandolin / E9 (with pedals) / C6 / banjo 5th string & ties?
+				String muting
+
+				Vanilla / Diatonic Mode
+				Triad I - VII
+				Seventh Chords I - VII
+				Diatonic scale / modes I - VII
+				Pentatonic I - VI
+
+				Parallel Sixths - string pair chooser E A D
+
+				Blues Mode - major / minor
+				I / IV / V
+				X Triad
+				X Dominant 7th
+				X Mixolydian
+				Pentatonic
+					X Major Pentatonic / Hexatonic
+					X Minor Pentatonic / Hexatonic
+
+					optional pentatonics I IV V
+				X Diad 3 & 7
+			*/}
+		</div>
+	);
+}
