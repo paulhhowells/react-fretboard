09f3d88e4f9188d2eced3d126362c7f20590ef28
Paul H Howells <p.h.howells@gmail.com>
2022-02-23 22:16:58 +0000

    feat: add octave markers

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -2,7 +2,7 @@ import { FRETBOARD_LEFT_PAD, FRET_WIDTH } from '../constants';
 
 const fretMarkerPositions = [
 	// 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
-	0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1,
+	0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 2,
 ];
 
 const adjustmentX = FRETBOARD_LEFT_PAD + (FRET_WIDTH / 2);
@@ -12,10 +12,17 @@ export default function Markers ({ frets, fretHeight }) {
 	const markers = frets.map(
 		(fret, index) => {
 			const markerX = adjustmentX + previousFret + ((fret - previousFret) / 2);
+			const position = fretMarkerPositions[index];
+			const marker = position > 0
+				? {
+					x: markerX,
+					...(position === 2 && { octave: true })
+				}
+				: false;
 
 			previousFret = fret;
 
-			return fretMarkerPositions[index] ? markerX : false;
+			return marker;
 		}
 	);
 
@@ -24,13 +31,30 @@ export default function Markers ({ frets, fretHeight }) {
 			{
 				markers.map(
 					(marker, index) => marker
-						? <circle
-							key={'marker' + index}
-							cx={marker}
-							cy={fretHeight}
-							r="2"
-							className="fretboard__marker"
-						/>
+						? marker.octave
+							? (
+								<g key={'marker' + index}>
+									<circle
+										cx={marker.x - 4}
+										cy={fretHeight}
+										r="2"
+										className="fretboard__marker" />
+									<circle
+										cx={marker.x + 4}
+										cy={fretHeight}
+										r="2"
+										className="fretboard__marker" />
+								</g>
+							)
+							: (
+								<circle
+									key={'marker' + index}
+									cx={marker.x}
+									cy={fretHeight}
+									r="2"
+									className="fretboard__marker"
+								/>
+							)
 						: ''
 				)
 			}
