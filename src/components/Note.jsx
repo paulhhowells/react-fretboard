1d213e8614df3cd5b3b76596642120fdc6ae9462
Paul H Howells <p.h.howells@gmail.com>
2022-02-23 22:32:39 +0000

    feat: signal root notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,19 @@
 const elementClassName = 'fretboard__note';
 
 export default function Note ({ note }) {
-	const { label, x, sign } = note;
+	const { degree, label, sign, x } = note;
 	const radius = 8;
-	const className = elementClassName + (
-		(sign.length > 0)
-			? ` ${elementClassName}--${sign}`
-			: ''
-	);
+	let className = elementClassName;
+
+	if (sign.length > 0) {
+		// Signal sharp or flat with a modifier.
+		className += ` ${elementClassName}--${sign}`;
+	}
+
+	if (degree === 1) {
+		// Signal it’s a root note.
+		className += ` ${elementClassName}--root`;
+	}
 
 	return (
 		<g transform={`translate(${ x })`} className={className}>
