73f81ee2dc47e48fba0164995f8852c13107f7e6
Paul H Howells <p.h.howells@gmail.com>
2022-02-23 22:30:13 +0000

    fix: remove roboto font
    because it does not include sharps or flats which thus have poor kerning

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<CssBaseline />
+		{/* <CssBaseline /> */}
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
